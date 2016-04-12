2f9a06d160f02ca5f8271f6625317a559c378eda
AditiSurana <suraa423@newschool.edu>
2016-04-12 18:23:16 -0400

    Pendulum created, no Gui

---
+++ Week_10/Pendulum/src/Pen.hpp
@@ -0,0 +1,31 @@
+
+
+#ifndef Pen_hpp
+#define Pen_hpp
+
+#include <stdio.h>
+#include "ofMain.h"
+
+#endif /* Pen_hpp */
+
+class Pen{
+public:
+    
+    float r;
+    float angle;
+    float aVel;
+    float aAcc;
+    float damping;
+    ofVec2f origin;
+    ofVec2f location;
+    float size;
+    
+public:
+    Pen();
+    Pen(ofVec2f, float, float);
+    void update();
+    void display();
+    
+    
+    
+};
